783d891c4d3f5c5e8a6054f3cd64e996cd85facf
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-09 11:47:51 +0100

    Add mango to fruits list

--- project/main.py
+++ project/main.py
@@ -5,5 +5,5 @@ def pick_a_fruit(fruits):
     return random.choice(fruits)
 
 if __name__ == '__main__':
-    fruits = ['banana', 'strawberry', 'blueberry', 'kiwi']
+    fruits = ['banana', 'strawberry', 'blueberry', 'kiwi', 'mango']
     print(pick_a_fruit(fruits))
