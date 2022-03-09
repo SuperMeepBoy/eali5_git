38f98d9ab933c3f364f4a7c9b41013c1dddffd22
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-09 11:47:24 +0100

    Add kiwi to fruits list

---
+++ project/main.py
@@ -0,0 +1,9 @@
+import random
+
+
+def pick_a_fruit(fruits):
+    return random.choice(fruits)
+
+if __name__ == '__main__':
+    fruits = ['banana', 'strawberry', 'blueberry', 'kiwi']
+    print(pick_a_fruit(fruits))
