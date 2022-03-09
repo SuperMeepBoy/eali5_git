3cb2fb947f953858e80e37440b4a814d9b5eeac0
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-09 11:50:07 +0100

    Add jujube to fruits list

--- project/main.py
+++ project/main.py
@@ -5,5 +5,5 @@ def pick_a_fruit(fruits):
     return random.choice(fruits)
 
 if __name__ == '__main__':
-    fruits = ['banana', 'strawberry', 'blueberry', 'kiwi', 'mango', 'grapes']
+    fruits = ['banana', 'strawberry', 'blueberry', 'kiwi', 'mango', 'grapes', 'jujube']
     print(pick_a_fruit(fruits))
