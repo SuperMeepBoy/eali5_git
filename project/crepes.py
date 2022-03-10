fd35db97a64bef2093ce73feb65ad3c54bf3a289
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-10 15:17:34 +0100

    Ajout modèle crepes

---
+++ project/crepes.py
@@ -0,0 +1,8 @@
+class Crepe():
+
+    def __init__(self):
+        self.ingredients = []
+        self.cuite = False
+
+    def ajouter_ingredient(self, ingredient):
+        self.ingredients.append(ingredient)
