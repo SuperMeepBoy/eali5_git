ff9f74c2eff4995f7e5ac8f82809aa0b96ebb8e9
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-10 15:30:32 +0100

    Ajout crepe confiture

--- project/crepes.py
+++ project/crepes.py
@@ -6,3 +6,8 @@ class Crepe():
 
     def ajouter_ingredient(self, ingredient):
         self.ingredients.append(ingredient)
+
+
+def ajouter_crepe_confiture():
+    crepe = Crepe()
+    crepe.ajouter_ingredient("confiture")
