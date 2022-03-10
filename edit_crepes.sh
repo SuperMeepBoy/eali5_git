db19f6e46ada53f408c2ee458e4cb852b88935af
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-10 15:27:12 +0100

    Add script to get modifications for exercice

---
+++ edit_crepes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+edited="class Crepe():
+
+    def __init__(self):
+        self.ingredients = []
+        self.cuite = False
+
+    def ajouter_ingredient(self, ingredient):
+        if self.cuite:
+            self.ingredients.append(ingredient)
+
+    def cuire(self):
+        self.cuite = True
+
+
+def ajout_crepe_nutella():
+    crepe = Crepe()
+    crepe.cuire()
+    crepe.ajouter_ingredient(\"nutella\")
+"
+
+echo "$edited" > project/crepes.py 
