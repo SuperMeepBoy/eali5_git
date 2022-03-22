06cd8d0f6bda19b700bf9c7e32168432c120e100
Jef Roelandt <roelandt.jef@protonmail.com>
2022-03-22 15:28:21 +0100

    Add square function

---
+++ project/main.py
@@ -0,0 +1,16 @@
+def multiply(nb1, nb2):
+    
+    def add(nb1, nb2):
+        # A compléter
+        pass
+
+    res = 0
+    for i in range(nb2):
+        res = add(res, nb1)
+    return res
+
+
+def square(nb):
+    # A compléter
+    # return multiply()
+    pass
